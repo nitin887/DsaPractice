14eee10a4e3ecd4c4eafeeeaad0bdd2b68114c7c
nitin887 <nitinrao9213@gmail.com>
2026-08-11 22:46:39 +0530

    rotate an array

--- String.cs
+++ String.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using System.IO.Compression;
 using System.Security.AccessControl;
 using System.Security.Cryptography.X509Certificates;
 
@@ -7,13 +8,32 @@ class String
     static void Main()
     {
         string name = "nitink";
-        for (int i = name.Length - 1; i >= 0; i--)
+        char[] name1 = name.ToCharArray();
+        int d = 2;
+
+        for (int i = 0; i < d; i++)
         {
-            Console.WriteLine(name[i]);
+            char first = name1[0];
+            for (int j = 0; j < name1.Length - 1; j++)
+            {
+                name1[j] = name1[j + 1];
 
 
-
+            }
+            name1[name1.Length - 1] = first;
 
         }
+
+
+        for (int i = 0; i < name1.Length; i++)
+        {
+            Console.WriteLine(name1[i]);
+
+        }
+
+
+
+
+
     }
 }
