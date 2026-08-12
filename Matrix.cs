199b0d434412f172723c1ad327a01a5df28f9d86
nitin887 <nitinrao9213@gmail.com>
2026-08-12 08:36:05 +0530

    traversing the matrix

---
+++ Matrix.cs
@@ -0,0 +1,19 @@
+class Matrix
+{
+    static void Main()
+    {
+        int[,] matrix = { { 1, 2, 3 }, { 2, 3, 4 }, { 4, 5, 6 } };
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                Console.Write(matrix[i, j]);
+
+
+
+
+            }
+
+        }
+    }
+}
