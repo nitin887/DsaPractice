b96de0d5ff0e6c31888de6756ed651c989f1aa8c
nitin887 <nitinrao9213@gmail.com>
2026-08-12 08:03:43 +0530

    rotating a string using reversal algorithm

--- String.cs
+++ String.cs
@@ -2,35 +2,45 @@ using System.ComponentModel.DataAnnotations;
 using System.IO.Compression;
 using System.Security.AccessControl;
 using System.Security.Cryptography.X509Certificates;
-
+/*
+Reversal algorithm
+*/
 class String
 {
+    static string Rotate(string name, int d)
+    {
+        int n = name.Length;
+        d %= n;
+        char[] temp = name.ToCharArray();
+        Reverse(temp, 0, d - 1);
+        Reverse(temp, d, n - 1);
+        Reverse(temp, 0, n - 1);
+
+        return new string(temp);
+
+    }
+
+    static void Reverse(char[] temp, int start, int end)
+    {
+        while (start < end)
+        {
+            (temp[start], temp[end]) = (temp[end], temp[start]);
+            start++;
+            end--;
+        }
+    }
+
     static void Main()
     {
         string name = "nitink";
-        char[] name1 = name.ToCharArray();
-        int d = 8;
-        d %= name1.Length;
-
-        for (int i = 0; i < d; i++)
-        {
-            char first = name1[0];
-            for (int j = 0; j < name1.Length - 1; j++)
-            {
-                name1[j] = name1[j + 1];
+        int d = 2;
+        Console.WriteLine(Rotate(name, d));
 
 
-            }
-            name1[name1.Length - 1] = first;
-
-        }
 
 
-        for (int i = 0; i < name1.Length; i++)
-        {
-            Console.WriteLine(name1[i]);
 
-        }
+
 
 
 
