d3b22ede16777e86af80254040f4278b3c005c0e
nitin887 <nitinrao9213@gmail.com>
2026-08-12 10:10:19 +0530

    search a number in the matrix

--- Matrix.cs
+++ Matrix.cs
@@ -3,17 +3,23 @@ class Matrix
     static void Main()
     {
         int[,] matrix = { { 1, 2, 3 }, { 2, 3, 4 }, { 4, 5, 6 } };
+        int x = 2;
+
         for (int i = 0; i < matrix.GetLength(0); i++)
         {
             for (int j = 0; j < matrix.GetLength(1); j++)
             {
-                Console.Write(matrix[i, j]);
+                if (matrix[i, j] == x)
+                {
+                    Console.WriteLine($"element exist at {i} {j} ");
 
-
-
-
+                }
             }
 
         }
+
+
+
+
     }
 }
